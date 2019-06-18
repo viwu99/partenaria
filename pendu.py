66abcf3e6f6cb142bc0a43f6652899c922be9c2b
victor <viwu@protonmail.com>
2019-06-18 02:36:13 +0200

    azdazd

--- pendu.py
+++ pendu.py
@@ -1,1 +1,82 @@
 import re
+import random
+import os
+
+
+def pendus():
+    pseudo = input('Entre ton pseudo : ')
+    listmot = ['a','a']
+    nbaléatoire = random.randint(0, len(listmot)-1)
+    motpendu = listmot[nbaléatoire]
+    motcaché = str()
+    for i in motpendu:
+        motcaché = motcaché + 'x'
+    print('Mot a déviner : ', motcaché)
+    nbdevie = 8
+    while nbdevie > 0:
+        lettre = input(f'Entré une lettre ')
+        x = 0 # nb de lettre trouvé
+        for i in range(0,len(motcaché)):
+            if lettre == motpendu[i]:
+                lmotcaché = list(motcaché)
+                lmotcaché[i] = motpendu[i]
+                motcaché = ''.join(lmotcaché)
+                x += 1
+        if x != 0:
+            print(f'bravo tu a trouvé {x}\n Le mot est {motcaché}')
+            if motcaché == motpendu: #si win
+                print(f'Bravo tu a gagnés tu marques donc {nbdevie} point(s)')
+                ajoutpoint(pseudo, nbdevie)
+                exit()
+        else:
+            print(f"Tu n'as pas trouvé de lettre il te reste {i} chance(s)")
+            nbdevie -= 1
+    print('Tu a perdu dommage')
+    exit()
+
+def ajoutpoint(pseudo,point):
+    print('pseudo,points ', pseudo, point)
+    #besoin d import os
+    currentpath = os.path.dirname(os.path.abspath(__file__)) + "/"
+    f = open(currentpath + 'datapendu.txt', "r") # f = fichier pendu
+    data = f.read()
+    f.close
+    lpseudo = re.findall('--(.*)--', data)
+    lpoint = re.findall('::(.*)::', data)
+    b = 0
+    print('lpoint', lpoint)
+    for i in range(0, len(lpseudo)):
+        print('hey')
+        if pseudo == lpseudo[i]: #si le joueur existe deja
+            lpoint[i] = int(lpoint[i]) + point
+            b = 1
+    if b ==0:              #s'il existe pas
+        lpseudo.append(pseudo)
+        lpoint.append(point)
+    lpp = list()  #liste pseudo points
+    a = 0
+    print(len(pseudo))
+    for i in range(0, len(pseudo)-1):  #crée la liste
+        a = 1
+        print(i)
+        lpp.append(f'--{lpseudo[i]}--')
+        lpp.append(f'::{lpoint[i]}::')
+
+    if a == 0: #premiere fois
+        lpseudo.append(pseudo)
+        lpoint.append(point)
+        lpp.append(f'--{lpseudo[0]}--')
+        lpp.append(f'::{lpoint[0]}::')
+
+    newdata = '\n'.join(lpp)
+    f = open(currentpath + 'datapendu.txt', "w")  # f = fichier pendu
+    f.write(newdata)
+    print('newdata : ',newdata, 'lpp',lpp)
+
+def main():
+    pendus()
+
+if __name__ == "__main__":
+    main()
+
+#motcaché = re.sub('(.)[a-z]', 'x', motpendu)
